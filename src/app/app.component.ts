bec900bcf19e93afea28573cbdd2d72647c3e470
oguzgelal <o.gelal77@gmail.com>
2016-09-24 18:47:46 +0200

    enable scss, integrate bootstrap and font-awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
-import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
-  items: FirebaseObjectObservable<any[]>;
-  constructor(private _af: AngularFire) {
-    this.items = _af.database.object('/items');
-  }
-
-
 }
