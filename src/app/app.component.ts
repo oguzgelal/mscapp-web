9a78bf446183dd056534c46b809f09d7a9313846
oguzgelal <o.gelal77@gmail.com>
2016-09-24 17:40:04 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+  items: FirebaseListObservable<any[]>;
+  constructor(private _af: AngularFire) {
+    this.items = _af.database.list('items');
+  }
+
+
+}
